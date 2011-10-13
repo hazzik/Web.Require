56ca33eab9aa55bc7a079bf487910082f132a2d4
hazzik <hazzik@gmail.com>
2011-10-13 22:37:20 +0600

    Bug with incorrect output "Value is not created." was fixed

---
+++ src/Web.Require.Tests/RequireHtmlHelperExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿namespace Brandy.Web.Require.Tests
+{
+    using System.Collections;
+    using System.Web.Mvc;
+    using Moq;
+    using Xunit;
+
+    public class RequireHtmlHelperExtensionsTests
+    {
+        private readonly HtmlHelper html = CreateHtmlHelper();
+
+        [Fact]
+        public void OutputRequiredStyleSheetsShouldNotWriteValueNotCreated()
+        {
+            Assert.DoesNotContain("Value is not created.", html.OutputRequiredStyleSheets().ToHtmlString());
+        }
+
+        [Fact]
+        public void OutputRequiredScriptsShouldNotWriteValueNotCreated()
+        {
+            Assert.DoesNotContain("Value is not created.", html.OutputRequiredScripts().ToHtmlString());
+        }
+
+        private static HtmlHelper CreateHtmlHelper()
+        {
+            var viewContextMock = new Mock<ViewContext>();
+            var viewDataContainerMock = new Mock<IViewDataContainer>();
+            viewContextMock.Setup(x => x.HttpContext.Items).Returns(new Hashtable());
+            return new HtmlHelper(viewContextMock.Object, viewDataContainerMock.Object);
+        }
+    }
+}
--- src/Web.Require/RequireHtmlHelperExtensions.cs
+++ src/Web.Require/RequireHtmlHelperExtensions.cs
@@ -1,118 +1,171 @@
 ﻿namespace Brandy.Web.Require
 {
-    using System;
+    using System.Linq;
     using System.Web;
     using System.Web.Mvc;
 
     public static class RequireHtmlHelperExtensions
     {
-        private static Lazy<Scripts> scripts = new Lazy<Scripts>();
-        private static Lazy<AsyncScripts> asyncScripts = new Lazy<AsyncScripts>();
-        private static Lazy<StyleSheets> styleSheets = new Lazy<StyleSheets>();
-
         public static string RequireScript(this HtmlHelper html, string path)
         {
-            if (asyncScripts.Value.Contains(path) == false)
-                scripts.Value.Add(path);
+            AsyncScripts asyncScripts = AsyncScripts(html);
+            Scripts scripts = Scripts(html);
+            if (asyncScripts.Contains(path) == false)
+                scripts.Add(path);
             return string.Empty;
         }
 
         public static string RequireScript(this HtmlHelper html, string path1, string path2)
         {
-            RequireScript(html, path1);
-            RequireScript(html, path2);
+            AsyncScripts asyncScripts = AsyncScripts(html);
+            Scripts scripts = Scripts(html);
+            if (asyncScripts.Contains(path1) == false)
+                scripts.Add(path1);
+            if (asyncScripts.Contains(path2) == false)
+                scripts.Add(path2);
             return string.Empty;
         }
 
         public static string RequireScript(this HtmlHelper html, string path1, string path2, string path3)
         {
-            RequireScript(html, path1);
-            RequireScript(html, path2);
-            RequireScript(html, path3);
+            AsyncScripts asyncScripts = AsyncScripts(html);
+            Scripts scripts = Scripts(html);
+            if (asyncScripts.Contains(path1) == false)
+                scripts.Add(path1);
+            if (asyncScripts.Contains(path2) == false)
+                scripts.Add(path2);
+            if (asyncScripts.Contains(path3) == false)
+                scripts.Add(path3);
             return string.Empty;
         }
 
         public static string RequireScript(this HtmlHelper html, params string[] paths)
         {
-            Scripts requiredScripts = scripts.Value;
-            foreach (string path in paths)
-                requiredScripts.Add(path);
+            Scripts scripts = Scripts(html);
+            AsyncScripts asyncScripts = AsyncScripts(html);
+            foreach (string path in paths.Where(path => asyncScripts.Contains(path) == false))
+                scripts.Add(path);
             return string.Empty;
         }
 
         public static string RequireScriptAsync(this HtmlHelper html, string path)
         {
-            if (scripts.Value.Contains(path) == false)
-                asyncScripts.Value.Add(path);
+            Scripts scripts = Scripts(html);
+            AsyncScripts asyncScripts = AsyncScripts(html);
+            if (scripts.Contains(path) == false)
+                asyncScripts.Add(path);
             return string.Empty;
         }
 
         public static string RequireScriptAsync(this HtmlHelper html, string path1, string path2)
         {
-            RequireScriptAsync(html, path1);
-            RequireScriptAsync(html, path2);
+            Scripts scripts = Scripts(html);
+            AsyncScripts asyncScripts = AsyncScripts(html);
+            if (scripts.Contains(path1) == false)
+                asyncScripts.Add(path1);
+            if (scripts.Contains(path2) == false)
+                asyncScripts.Add(path2);
             return string.Empty;
         }
 
         public static string RequireScriptAsync(this HtmlHelper html, string path1, string path2, string path3)
         {
-            RequireScriptAsync(html, path1);
-            RequireScriptAsync(html, path2);
-            RequireScriptAsync(html, path3);
+            Scripts scripts = Scripts(html);
+            AsyncScripts asyncScripts = AsyncScripts(html);
+            if (scripts.Contains(path1) == false)
+                asyncScripts.Add(path1);
+            if (scripts.Contains(path2) == false)
+                asyncScripts.Add(path2);
+            if (scripts.Contains(path3) == false)
+                asyncScripts.Add(path3);
             return string.Empty;
         }
 
         public static string RequireScriptAsync(this HtmlHelper html, params string[] paths)
         {
-            foreach (string path in paths)
-                RequireScriptAsync(html, path);
+            Scripts scripts = Scripts(html);
+            AsyncScripts asyncScripts = AsyncScripts(html);
+            foreach (string path in paths.Where(path => scripts.Contains(path) == false))
+                asyncScripts.Add(path);
             return string.Empty;
         }
 
         public static string RequireStyleSheet(this HtmlHelper html, string path)
         {
-            styleSheets.Value.Add(path);
+            StyleSheets(html).Add(path);
             return string.Empty;
         }
 
         public static string RequireStyleSheet(this HtmlHelper html, string path1, string path2)
         {
-            StyleSheets requiredStyleSheets = styleSheets.Value;
-            requiredStyleSheets.Add(path1);
-            requiredStyleSheets.Add(path2);
+            StyleSheets styleSheets = StyleSheets(html);
+            styleSheets.Add(path1);
+            styleSheets.Add(path2);
             return string.Empty;
         }
 
         public static string RequireStyleSheet(this HtmlHelper html, string path1, string path2, string path3)
         {
-            StyleSheets requiredStyleSheets = styleSheets.Value;
-            requiredStyleSheets.Add(path1);
-            requiredStyleSheets.Add(path2);
-            requiredStyleSheets.Add(path3);
+            StyleSheets styleSheets = StyleSheets(html);
+            styleSheets.Add(path1);
+            styleSheets.Add(path2);
+            styleSheets.Add(path3);
             return string.Empty;
         }
 
         public static string RequireStyleSheet(this HtmlHelper html, params string[] paths)
         {
-            StyleSheets requiredStyleSheets = styleSheets.Value;
+            StyleSheets styleSheets = StyleSheets(html);
             foreach (string path in paths)
-                requiredStyleSheets.Add(path);
+                styleSheets.Add(path);
             return string.Empty;
         }
 
         public static IHtmlString OutputRequiredScripts(this HtmlHelper html)
         {
-            MvcHtmlString requiredScripts = MvcHtmlString.Create(string.Format("{0}{1}", scripts, asyncScripts.Value.ToString(html)));
-            scripts = new Lazy<Scripts>();
-            asyncScripts = new Lazy<AsyncScripts>();
+            MvcHtmlString requiredScripts = MvcHtmlString.Create(string.Format("{0}{1}", Scripts(html), AsyncScripts(html).ToString(html)));
+            RemoveContainer<Scripts>(html);
+            RemoveContainer<AsyncScripts>(html);
             return requiredScripts;
         }
 
         public static IHtmlString OutputRequiredStyleSheets(this HtmlHelper html)
         {
-            MvcHtmlString requiredStyleSheets = MvcHtmlString.Create(styleSheets.ToString());
-            styleSheets = new Lazy<StyleSheets>();
+            MvcHtmlString requiredStyleSheets = MvcHtmlString.Create(StyleSheets(html).ToString());
+            RemoveContainer<StyleSheets>(html);
             return requiredStyleSheets;
+        }
+
+        private static Scripts Scripts(HtmlHelper html)
+        {
+            return GetOrCreateContainer<Scripts>(html);
+        }
+
+        private static AsyncScripts AsyncScripts(HtmlHelper html)
+        {
+            return GetOrCreateContainer<AsyncScripts>(html);
+        }
+
+        private static StyleSheets StyleSheets(HtmlHelper html)
+        {
+            return GetOrCreateContainer<StyleSheets>(html);
+        }
+
+        private static T GetOrCreateContainer<T>(HtmlHelper html)
+            where T : class, IAssetsContainer, new()
+        {
+            HttpContextBase httpContext = html.ViewContext.HttpContext;
+            T container = (T) httpContext.Items[typeof (T)] ?? new T();
+            httpContext.Items[typeof (T)] = container;
+            return container;
+        }
+
+        private static T RemoveContainer<T>(HtmlHelper html)
+            where T : class, IAssetsContainer, new()
+        {
+            HttpContextBase httpContext = html.ViewContext.HttpContext;
+            httpContext.Items[typeof (T)] = null;
+            return null;
         }
     }
 }
