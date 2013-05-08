edc3fc96bc65620c30984c609472474f58b6baea
Alexander I. Zaytsev <hazzik@gmail.com>
2013-05-09 01:59:29 +1200

    Key

--- src/Web.Require/Properties/AssemblyInfo.cs
+++ src/Web.Require/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Web.Require")]
 [assembly: Guid("7a3d5a75-761f-497c-ae54-4b483a5fc3ba")]
-[assembly: InternalsVisibleTo("Brandy.Web.Require.Tests")]
+[assembly: InternalsVisibleTo("Brandy.Web.Require.Tests, PublicKey = 0024000004800000940000000602000000240000525341310004000001000100e18c8f02957438c82dcb37781f5b79cacb892aed640df567731c7c8e8e6fffc4f7dcddae236b156e9662ff5a2f35dfa4155ff1b3962b1574fb46c3e5e7ebeedb3f1abbcc6e2ad6eeecd7a832b6f66ce2b06b35ec8bbe6fc651ba4b907fc779d00471b99dc2cd4fe8bb79dcf66346611e12b6b8138de80b70a6013b5e07395ee4")]
